93e22ca98f7b4bf1f5161520800fbc4cc851ef7b
AdnanChowdhury7249 <adnanchowdhury7249@gmail.com>
2025-10-06 11:03:31 +0100

    created file which builds the point series coverJson

---
+++ convertToCoverageJSON.py
@@ -0,0 +1,54 @@
+from typing import List, Dict
+from read_csv import Weather_Data
+
+
+def convert_to_pointseries_covjson(rows: List[Weather_Data]):
+    temps = [r.temperature for r in rows]
+    times = [r.time for r in rows]
+    lon = rows[0].lon
+    lat = rows[0].lat
+
+    return {
+        "type": "Coverage",
+        "domain": {
+            "type": "Domain",
+            "domainType": "PointSeries",
+            "axes": {
+                "x": {"values": [lon]},
+                "y": {"values": [lat]},
+                "t": {"values": times},
+            },
+            "referencing": [
+                {
+                    "coordinates": ["x", "y"],
+                    "system": {
+                        "type": "GeographicCRS",
+                        "id": "http://www.opengis.net/def/crs/EPSG/0/4326",
+                    },
+                },
+                {
+                    "coordinates": ["t"],
+                    "system": {"type": "TemporalRS", "calendar": "Gregorian"},
+                },
+            ],
+        },
+        "parameters": {
+            "temperature": {
+                "type": "Parameter",
+                "observedProperty": {
+                    "id": "http://vocab.nerc.ac.uk/standard_name/air_temperature/",
+                    "label": {"en": "Air temperature"},
+                },
+                "unit": {"label": {"en": "kelvin"}, "symbol": {"value": "K"}},
+            }
+        },
+        "ranges": {
+            "temperature": {
+                "type": "NdArray",
+                "dataType": "float",
+                "axisNames": ["t"],
+                "shape": [len(temps)],
+                "values": temps,
+            }
+        },
+    }
